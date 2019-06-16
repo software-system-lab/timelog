43880ac478214687dccb5e595a1c2a086964079b
胡喻翔 <huyuxiang@huyuxiangs-MacBook-Pro.local>
2019-06-16 08:15:16 +0800

    Fix display name

--- src/components/_Header.vue
+++ src/components/_Header.vue
@@ -1,69 +1,67 @@
 <template>
-  <el-menu>
-    <img src="../../static/image/timelog.png" alt="logo">
-    <img alt="profile img" :src="this.profilePicture" class="img-circle" />
-    <h4>{{this.profileName}}</h4>
-    <router-link :to="{ name: 'Board'}">
-      <el-menu-item index="1">
-        <i class="el-icon-edit"></i>
-        <span slot="title">Board</span>
-      </el-menu-item>
-    </router-link>
-    <router-link :to="{ name: 'History'}">
-      <el-menu-item index="2">
-        <i class="el-icon-document"></i>
-        <span slot="title">History</span>
-      </el-menu-item>
-    </router-link>
-    <router-link :to="{ name: 'Team'}">
-      <el-menu-item index="3">
-        <i class="el-icon-menu"></i>
-        <span slot="title">Team</span>
-      </el-menu-item>
-    </router-link>
-    <router-link :to="{ name: 'Setting'}">
-      <el-menu-item index="4">
-        <i class="el-icon-setting"></i>
-        <span slot="title">Setting</span>
-      </el-menu-item>
-    </router-link>
+<el-menu>
+  <img src="../../static/image/timelog.png" alt="logo">
+  <img alt="profile img" :src="this.profilePicture" class="img-circle" />
+  <h4>{{this.profileName}}</h4>
+  <router-link :to="{ name: 'Board'}">
+    <el-menu-item index="1">
+      <i class="el-icon-edit"></i>
+      <span slot="title">Board</span>
+    </el-menu-item>
+  </router-link>
+  <router-link :to="{ name: 'History'}">
+    <el-menu-item index="2">
+      <i class="el-icon-document"></i>
+      <span slot="title">History</span>
+    </el-menu-item>
+  </router-link>
+  <router-link :to="{ name: 'Team'}">
+    <el-menu-item index="3">
+      <i class="el-icon-menu"></i>
+      <span slot="title">Team</span>
+    </el-menu-item>
+  </router-link>
+  <router-link :to="{ name: 'Setting'}">
+    <el-menu-item index="4">
+      <i class="el-icon-setting"></i>
+      <span slot="title">Setting</span>
+    </el-menu-item>
+  </router-link>
 
-  </el-menu>
+</el-menu>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
+export default {
+  data() {
+    return {
 
-      }
+    }
+  },
+  computed: {
+    profileName() {
+      return window.Profile.UserName;
     },
-    computed: {
-      profileName() {
-        return window.FBProfile.name;
-      },
-      profilePicture() {
-        return `https://graph.facebook.com/${window.FBProfile.id}/picture?width=300`;
-      }
-    },
-  }
-
+    profilePicture() {
+      return `https://graph.facebook.com/${window.FBProfile.id}/picture?width=300`;
+    }
+  },
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  img {
-    width: 80px;
-    display: block;
-    margin: auto;
-  }
+img {
+  width: 80px;
+  display: block;
+  margin: auto;
+}
 
-  .img-circle {
-    border-radius: 50%;
-  }
+.img-circle {
+  border-radius: 50%;
+}
 
-  a {
-    text-decoration: none;
-  }
-
+a {
+  text-decoration: none;
+}
 </style>
