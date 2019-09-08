283e5e03abe6edd9a735b545f3a3ac8cf258df0a
Patrick <nightlord851108@gmail.com>
2019-09-08 22:33:41 +0800

    Config files updated

---
+++ server/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  DBconnection: {
+    host: MARIA_HOST,
+    user: MARIA_USER,
+    password: MARIA_PASSWD,
+    database: MARIA_DB,
+    timezone: "08:00"
+  },
+  FBAppAccessToken: {
+    appId: FB_ID,
+    appSecrete: FB_SECRETE,
+  }
+}
---
+++ src/config.js
@@ -0,0 +1,12 @@
+export default {
+  FBLogin: {
+    appId: 216015235921191,
+    cookie: true,
+    xfbml: true,
+    version: 'v3.1'
+  },
+  apiDest: {
+    host: 'ssl-timelog.csie.ntut.edu.tw',
+    port: '443'
+  }
+}
