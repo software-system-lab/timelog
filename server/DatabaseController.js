8e5de19fb599d05af36ce608fb43816521b5d54a
Patrick <nightlord851108@gmail.com>
2019-10-04 16:26:49 +0800

    Revert "Use mariadb instead of mysql"
    This reverts commit 710e24210e1c4cc17ab8eae6801cc7f5c34c4bed.

--- server/DatabaseController.js
+++ server/DatabaseController.js
@@ -1,26 +1,25 @@
-const mariadb = require('mariadb');
+const mysql = require('mysql');
 const Config = require('./config.js');
 
 // 與資料庫連線
-const pool = mariadb.createPool(Config.DBconnection);
+const pool = mysql.createPool(Config.DBconnection);
 
 let query = function (sql, values) {
   return new Promise((resolve, reject) => {
-    pool.getConnection()
-      .then( conn => {
-        conn.query(sql, values)
-          .then( res => {
-            console.log(res);
-            conn.end();
-          })
-          .catch( err => {
-            console.log(err);
-            conn.end();
-          })
-      })
-      .err( err => {
-        console.log("DB connection failed: ", err);
-      })
+    pool.getConnection(function (err, connection) {
+      if (err) {
+        reject(err)
+      } else {
+        connection.query(sql, values, (err, rows) => {
+          if (err) {
+            reject(err)
+          } else {
+            resolve(rows)
+          }
+          connection.release()
+        })
+      }
+    })
   })
 }
 
