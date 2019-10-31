e794b4091868823025fad46790e589a4f92f4a61
Patrick <nightlord851108@gmail.com>
2019-10-31 16:40:43 +0800

    Remove unused button

--- src/components/Board/goal.vue
+++ src/components/Board/goal.vue
@@ -1,7 +1,6 @@
 <template>
   <el-card>
     <div slot="header" class="clearfix">
-      <el-button type="primary" id='iteration-setting' icon="el-icon-setting" @click='setting'></el-button>
       <h2>My Iteration Target</h2>
     </div>
     <TargetBase v-for="project in projectList" :key="project.ProjectID" v-if="project.ProjectID != null" :project="project"></TargetBase>
@@ -14,11 +13,6 @@ import TargetBase from '@/components/Board/TargetBase'
 export default {
   props: {
     projectList: Array
-  },
-  methods: {
-    setting() {
-      this.$emit("setting")
-    }
   },
   components: {
     TargetBase
