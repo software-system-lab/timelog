d8adbd5c35985068247a155521e09eac781eab82
Patrick <nightlord851108@gmail.com>
2019-11-02 17:51:19 +0800

    Goal board should not show project with goal time is 0.

--- src/components/Board/goal.vue
+++ src/components/Board/goal.vue
@@ -3,7 +3,7 @@
     <div slot="header" class="clearfix">
       <h2>My Iteration Target</h2>
     </div>
-    <TargetBase v-for="project in projectList" :key="project.ProjectID" v-if="project.ProjectID != null" :project="project"></TargetBase>
+    <TargetBase v-for="project in projectList" :key="project.ProjectID" v-if="project.ProjectID != null && project.GoalHour > 0" :project="project"></TargetBase>
   </el-card>
 </template>
 
