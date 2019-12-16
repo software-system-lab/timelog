c06ba06d0a80872764aa967d918e5a63faf46d53
Patrick <nightlord851108@gmail.com>
2019-12-16 11:43:00 +0800

    Try to refactor Board/index.vue as class based component

--- src/components/Board/index.vue
+++ src/components/Board/index.vue
@@ -10,69 +10,84 @@
 </template>
 
 <script>
+import { Vue, Component, Prop } from 'vue-property-decorator'
 import Iteration from '@/components/Board/iteration/index.vue'
 import SpentTime from '@/components/Board/spent_time.vue'
 import Goal from '@/components/Board/goal.vue'
 import logService from '@/services/LogService.js'
 import profileService from '@/services/ProfileService.js'
 
-export default {
-  data() {
-    return {
-      iterationInfo: {
-        iterationID: null
-      },
-      projectList: [],
-      iterationSetting: false,
-      goalDisplay: true
-    }
-  },
+@Component({
+  components: {
+    Iteration,
+    SpentTime,
+    Goal
+ }
+})
+export default class Board extends Vue {
+  // Data members
+  iterationInfo = {
+    iterationID: null
+  };
+  projectList = [];
+  iterationSetting = false;
+  goalDisplay = true;
+
+
+  // ComponentsΩ
+
+
+  // Life cycle
+
   async created() {
     const iterationID = await profileService.getCurrentIteration();
     this.iterationInfo = await profileService.GetIterationById(iterationID)
     this.getProjectsData()
     this.$refs.iteration.iterationDate();
-  },
-  methods: {
-    async getProjectList() {
-      const userID = window.Profile.UserID
-      let result = await logService.projectTimeByIteration(userID, this.iterationInfo.IterationID)
-      if (result != "no data") {
-        this.projectList = result
-      }
-    },
-    async getProjectsData() {
-      await this.getProjectList()
-      this.$refs.spentTime.update(this.projectList)
-    },
-    update() {
-      this.goalDisplay = true
-      this.getProjectsData()
-      this.$refs.iteration.update()
-    },
-    async changeIteration(iterationID) {
-      this.iterationInfo = await profileService.GetIterationById(iterationID)
-      this.update()
-    },
-    openIterationSetting() {
-      this.iterationSetting = true
-    },
-    closeIterationSetting() {
-      this.iterationSetting = false
-      this.update()
-    },
-    async displayByDate(date) {
-      this.goalDisplay = false
-      this.iterationInfo.IterationID = ""
-      const userID = window.Profile.UserID
-      this.projectList = await logService.projectTime(userID, date.start, date.end)
-      this.$refs.spentTime.update(this.projectList)
+  }
+
+
+  // Methods
+
+  async getProjectList() {
+    const userID = window.Profile.UserID;
+    let result = await logService.projectTimeByIteration(userID, this.iterationInfo.IterationID);
+    if (result != "no data") {
+      this.projectList = result;
     }
-  },
-  components: {
-    Iteration,
-    SpentTime,
-    Goal
+  }
+
+  async getProjectsData() {
+    await this.getProjectList();
+    this.$refs.spentTime.update(this.projectList);
+  }
+
+  update() {
+    this.goalDisplay = true;
+    this.getProjectsData();
+    this.$refs.iteration.update();
+  }
+
+  async changeIteration(iterationID) {
+    this.iterationInfo = await profileService.GetIterationById(iterationID);
+    this.update();
+  }
+
+  openIterationSetting() {
+    this.iterationSetting = true;
+  }
+
+  closeIterationSetting() {
+    this.iterationSetting = false;
+    this.update();
+  }
+
+  async displayByDate(date) {
+    this.goalDisplay = false;
+    this.iterationInfo.IterationID = "";
+    const userID = window.Profile.UserID;
+    this.projectList = await logService.projectTime(userID, date.start, date.end);
+    this.$refs.spentTime.update(this.projectList);
   }
 }
 </script>
