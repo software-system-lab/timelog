93bb132b199500282fd38276b8757b97055c1c89
timchang1005 <timchang1005@gmail.com>
2020-02-14 15:11:51 +0800

    make sidebar fixed and make edit button disabled when no time box selected

--- src/App.vue
+++ src/App.vue
@@ -12,17 +12,31 @@ export default {
 </script>
 
 <style>
+
+html, body {
+  height: 100%;
+  margin: 0;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  height: 100%;
 }
 
 h1 {
   font-size: 40px;
+}
+
+#app > div {
+  height: 100%;
+}
+
+#app > div > .el-container {
+  height: 100%;
 }
 
 </style>
