3f3b556821bdc2845794f21999d0a755f1577eeb
Regina <ycycchre@gmail.com>
2020-03-09 23:01:30 +0800

    Add export file

--- src/components/publish/index.vue
+++ src/components/publish/index.vue
@@ -6,7 +6,7 @@
     <el-table :data="userDataList" sortable="true">
       <el-table-column type="expand">
         <template slot-scope="scope">
-          <SpentTime :LogReportData="scope.row.activityList" :publishVisible="false"/>
+          <SpentTime :LogReportData="scope.row.activityList" :buttonVisible="false"/>
         </template>
       </el-table-column>
       <el-table-column prop="name" label="Name" align="center">
