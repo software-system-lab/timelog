d15708487bc94db066eefed191c5a3aed4dc5135
Patrick <nightlord851108@gmail.com>
2020-03-10 11:33:24 +0800

    Use google pie chart

--- src/components/Board/spent_time.vue
+++ src/components/Board/spent_time.vue
@@ -2,10 +2,10 @@
   <el-card>
     <el-row>
       <h2>Spent Time</h2>
-      <h3>{{getHour(pieData.datasets[0].timeLength)}} : {{getMinute(pieData.datasets[0].timeLength)}}</h3>
+      <h3>{{getHour(totalTime)}} : {{getMinute(totalTime)}}</h3>
       <el-col :md="12" :sm="24">
         <div class="chart-container" style="margin: auto;">
-          <canvas id="Chart"></canvas>
+          <div :id="'piechart-' + chartID" style="width: 100%; height: 100%; min-height: 350px"></div>
         </div>
       </el-col>
       <el-col :md="12" :sm="24">
@@ -33,15 +33,15 @@
 </template>
 
 <script>
-import $ from 'jquery'
+/* global google */
 import { LogComponent } from '@/components/interface.js'
 import { Component, Watch } from 'vue-property-decorator'
-import Chart from 'chart.js'
 
 @Component({
   props: {
     LogReportData: Array,
-    publishVisible: Boolean
+    publishVisible: Boolean,
+    chartID: Number
   }
 })
 export default class SpentTime extends LogComponent {
@@ -50,92 +50,54 @@ export default class SpentTime extends LogComponent {
   pieChart = null
   logReportData = []
   pieData = this.initPieData()
+  totalTime = 0
+  chart = null
 
   @Watch('LogReportData')
   logReportDataChanged (newData) {
     this.logReportData = newData
-    this.setPieChart()
+    this.totalTime = 0
+    this.logReportData.forEach((item) => {
+      this.totalTime += item.TimeLength
+    })
+
+    if (this.chart) {
+      this.drawChart()
+    }
   }
 
   // Life cycle
   async mounted () {
-    this.ctx = $('#Chart')
     if (this.LogReportData) {
       this.logReportData = this.LogReportData
     }
+    google.charts.load('current', { packages: ['corechart'] })
+    google.charts.setOnLoadCallback(this.drawChart)
   }
 
   // Methods
-  serializePieData () {
-    this.pieData = this.initPieData()
-    this.pieData.labels.length = 0
-    this.pieData.datasets[0].data.length = 0
-    const maxLabelNums = 7
-    for (let i = 0; i < this.logReportData.length; i++) {
-      if (i < maxLabelNums) {
-        this.pieData.labels.push(this.logReportData[i].ActivityName)
-        this.pieData.datasets[0].data.push(this.logReportData[i].TimeLength.toFixed(0))
-      } else if (i === maxLabelNums) {
-        this.pieData.labels.push('Other Activities')
-        this.pieData.datasets[0].data.push(this.logReportData[i].TimeLength.toFixed(0))
-      } else {
-        this.pieData.datasets[0].data[5] = (parseInt(this.pieData.datasets[0].data[5]) + this.logReportData[i].TimeLength).toFixed(
-          0)
-      }
-      this.pieData.datasets[0].timeLength += this.logReportData[i].TimeLength
-    }
-
-    // data to hour
-    for (let i = 0; i < this.pieData.datasets[0].data.length; i++) {
-      this.pieData.datasets[0].data[i] = (this.pieData.datasets[0].data[i] / 3600000).toFixed(2)
-    }
-  }
-
-  initPieData () {
-    return {
-      labels: [],
-      datasets: [{
-        timeLength: 0,
-        data: [],
-        backgroundColor: [
-          'rgba(6, 71, 212, 0.7)',
-          'rgba(255, 206, 86, 0.7)',
-          'rgba(75, 192, 192, 0.7)',
-          'rgba(153, 102, 255, 0.7)',
-          'rgba(255, 145, 64, 0.7)',
-          'rgba(0, 199, 239, 0.7)',
-          'rgba(255, 99, 132, 0.7)'
-        ]
-      }]
-    }
-  }
-
-  generatePieChart () {
-    if (this.pieChart) {
-      this.pieChart.destroy()
-    }
-    this.pieChart = new Chart(this.ctx, {
-      type: 'pie',
-      data: this.pieData,
-      options: {
-        title: {
-          display: true,
-          text: '(hour)'
-        },
-        // showAllTooltips: true,
-        tooltips: {
-          callbacks: {
-            label: (tooltipItem, data) => {
-              var dataset = data.datasets[tooltipItem.datasetIndex]
-
-              var value = parseFloat(dataset.data[tooltipItem.index])
-              var currentValue = parseInt(value) + ':' + Math.round((value % 1) * 60)
-              return ' ' + data.labels[tooltipItem.index] + ': ' + currentValue
-            }
-          }
+  drawChart () {
+    const pieArray = [['Activity', 'Hours']]
+    this.logReportData.forEach((item) => {
+      pieArray.push([
+        item.ActivityName,
+        {
+          v: item.TimeLength / 3600000,
+          f: this.getHour(item.TimeLength) + ':' + this.getMinute(item.TimeLength)
         }
-      }
+      ])
     })
+
+    var data = google.visualization.arrayToDataTable(pieArray)
+
+    var options = {
+      title: 'Spent Time',
+      pieSliceText: 'value-and-percentage'
+    }
+
+    this.chart = new google.visualization.PieChart(document.getElementById('piechart-' + this.chartID))
+
+    this.chart.draw(data, options)
   }
 
   paddingLeft (str, len) {
