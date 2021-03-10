10912426030f7116a6178b9c22a485f57c474571
Sandy <c20m76z@gmail.com>
2021-03-10 14:45:30 +0800

    merge web and server

---
+++ server/src/main/resources/public/precache-manifest.cbdb1f24b9468d618b3d995c54f2f4b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95f1ebfaff57ebd3818190f740085066",
+    "url": "/index.html"
+  },
+  {
+    "revision": "64efd4836714cb21ac0e",
+    "url": "/static/css/main.8b3bf338.chunk.css"
+  },
+  {
+    "revision": "11dae42d15e7384fcf81",
+    "url": "/static/js/2.4fafb366.chunk.js"
+  },
+  {
+    "revision": "0bf38cf6c0b51ec69d7a52e8875a6dbf",
+    "url": "/static/js/2.4fafb366.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64efd4836714cb21ac0e",
+    "url": "/static/js/main.6eae677b.chunk.js"
+  },
+  {
+    "revision": "509a3d55c9865eeedf4f",
+    "url": "/static/js/runtime-main.968ddc30.js"
+  }
+]);
